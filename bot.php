acd10acd2b2674b7dd0d8b60f1a56b945186c1fd
EL MEHDI HABIEB <40608190+ELMEHDIHABIEB@users.noreply.github.com>
2024-06-08 00:26:55 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,10 +2,37 @@
 require 'vendor/autoload.php';
 
 use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
 
 $botToken = getenv('TELEGRAM_BOT_TOKEN');
 $chatId = getenv('TELEGRAM_CHAT_ID');
 $client = new Client();
+
+function getRandomProduct($client) {
+    $url = "https://www.aliexpress.com/wholesale?catId=0&initiative_id=SB_20220101000000&SearchText=smartphone";
+    $response = $client->get($url);
+    $html = $response->getBody()->getContents();
+
+    $crawler = new Crawler($html);
+    $products = $crawler->filter('.manhattan--container--1lP57Ag');
+    
+    if ($products->count() == 0) {
+        return null;
+    }
+    
+    $randomIndex = rand(0, $products->count() - 1);
+    $product = $products->eq($randomIndex);
+    
+    $productLink = "https:" . $product->filter('a')->attr('href');
+    $productTitle = $product->filter('h1')->text();
+    $productPrice = $product->filter('.manhattan--price--2hbO9a')->text();
+    
+    return [
+        'title' => $productTitle,
+        'link' => $productLink,
+        'price' => $productPrice
+    ];
+}
 
 function sendMessage($client, $botToken, $chatId, $message) {
     $url = "https://api.telegram.org/bot{$botToken}/sendMessage";
@@ -13,31 +40,20 @@ function sendMessage($client, $botToken, $chatId, $message) {
         'form_params' => [
             'chat_id' => $chatId,
             'text' => $message,
-            'parse_mode' => 'Markdown'
+            'parse_mode' => 'Markdown',
+            'disable_web_page_preview' => true
         ]
     ]);
 
     return $response;
 }
 
-function sendPhoto($client, $botToken, $chatId, $photoUrl) {
-    $url = "https://api.telegram.org/bot{$botToken}/sendPhoto";
-    $response = $client->post($url, [
-        'form_params' => [
-            'chat_id' => $chatId,
-            'photo' => $photoUrl,
-            'caption' => 'Sample Image from Google'
-        ]
-    ]);
+$product = getRandomProduct($client);
 
-    return $response;
+if ($product) {
+    $message = "*{$product['title']}*\n\nPrice: {$product['price']}\n[Buy Now]({$product['link']})";
+    $response = sendMessage($client, $botToken, $chatId, $message);
+    echo $response->getBody();
+} else {
+    echo "No products found.";
 }
-
-$message = "Hello World!";
-$photoUrl = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png";
-
-$responseMessage = sendMessage($client, $botToken, $chatId, $message);
-$responsePhoto = sendPhoto($client, $botToken, $chatId, $photoUrl);
-
-echo "Message response: " . $responseMessage->getBody();
-echo "Photo response: " . $responsePhoto->getBody();
