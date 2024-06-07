302e9dde6a78ceefe7020b2c5fd51b4700ef89a3
EL MEHDI HABIEB <40608190+ELMEHDIHABIEB@users.noreply.github.com>
2024-06-07 23:58:51 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,37 +2,10 @@
 require 'vendor/autoload.php';
 
 use GuzzleHttp\Client;
-use Symfony\Component\DomCrawler\Crawler;
 
 $botToken = getenv('TELEGRAM_BOT_TOKEN');
 $chatId = getenv('TELEGRAM_CHAT_ID');
 $client = new Client();
-
-function getRandomProduct($client) {
-    $url = "https://www.aliexpress.com/wholesale?catId=0&initiative_id=SB_20220101000000&SearchText=smartphone";
-    $response = $client->get($url);
-    $html = $response->getBody()->getContents();
-
-    $crawler = new Crawler($html);
-    $products = $crawler->filter('.manhattan--container--1lP57Ag');
-    
-    if ($products->count() == 0) {
-        return null;
-    }
-    
-    $randomIndex = rand(0, $products->count() - 1);
-    $product = $products->eq($randomIndex);
-    
-    $productLink = "https:" . $product->filter('a')->attr('href');
-    $productTitle = $product->filter('h1')->text();
-    $productImage = $product->filter('img')->attr('src');
-    
-    return [
-        'title' => $productTitle,
-        'link' => $productLink,
-        'image' => $productImage
-    ];
-}
 
 function sendMessage($client, $botToken, $chatId, $message) {
     $url = "https://api.telegram.org/bot{$botToken}/sendMessage";
@@ -47,12 +20,24 @@ function sendMessage($client, $botToken, $chatId, $message) {
     return $response;
 }
 
-$product = getRandomProduct($client);
+function sendPhoto($client, $botToken, $chatId, $photoUrl) {
+    $url = "https://api.telegram.org/bot{$botToken}/sendPhoto";
+    $response = $client->post($url, [
+        'form_params' => [
+            'chat_id' => $chatId,
+            'photo' => $photoUrl,
+            'caption' => 'Sample Image from Google'
+        ]
+    ]);
 
-if ($product) {
-    $message = "*{$product['title']}*\n\nCheck it out [here]({$product['link']})!\n\n![Product Image]({$product['image']})";
-    $response = sendMessage($client, $botToken, $chatId, $message);
-    echo $response->getBody();
-} else {
-    echo "No products found.";
+    return $response;
 }
+
+$message = "Hello World!";
+$photoUrl = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png";
+
+$responseMessage = sendMessage($client, $botToken, $chatId, $message);
+$responsePhoto = sendPhoto($client, $botToken, $chatId, $photoUrl);
+
+echo "Message response: " . $responseMessage->getBody();
+echo "Photo response: " . $responsePhoto->getBody();
